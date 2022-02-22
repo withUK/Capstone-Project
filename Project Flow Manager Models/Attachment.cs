18998c45a71ff21579ddf22bf6dd719b8ddccd00
withUK <peter.withington@live.co.uk>
2022-02-22 09:31:29 +0000

    Updated model to include the file type value

--- Project Flow Manager Models/Attachment.cs
+++ Project Flow Manager Models/Attachment.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public string Filename { get; set; }
+        public string Type { get; set; }
         public string Filepath { get; set; }
         public int Filesize { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
