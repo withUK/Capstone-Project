4fed4133abb4b28441b754f161f0fcd78de2275d
withUK <peter.withington@live.co.uk>
2022-05-01 22:10:06 +0100

    Began to outline the first method for the document conversion.

---
+++ Project Flow Manager/Helpers/ConversionHelper.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Pdf;
+
+namespace Project_Flow_Manager.Helpers
+{
+    public class ConversionHelper
+    {
+        private FileStream _documentStream;
+        private DocIORenderer _render = new DocIORenderer();
+        private PdfDocument _pdfDocument;
+        private MemoryStream outputStream;
+
+        public PdfDocument ConvertWordToPDF(string FilePath)
+        {
+            WordDocument wordDocument = new WordDocument(_documentStream, Syncfusion.DocIO.FormatType.Automatic);
+
+            return _pdfDocument;
+        }
+    }
+}
