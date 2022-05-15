8a4eab7bd87769479621b3396b10f9015ebe42e4
withUK <peter.withington@live.co.uk>
2022-05-15 18:23:16 +0100

    Index views and method created in DecisionController. Tile added to main index view.

---
+++ Project Flow Manager/Controllers/DecisionsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project_Flow_Manager.Controllers
+{
+    public class DecisionsController : Controller
+    {
+        private readonly InnovationManagerContext _context;
+        private readonly ProjectFlowAdministrationContext _adminContext;
+
+        public DecisionsController(InnovationManagerContext context, ProjectFlowAdministrationContext adminContext)
+        {
+            _context = context;
+            _adminContext = adminContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "Decisions";
+            return View(await _context.ProjectAssessmentReport.Include(i => i.Innovation).ToListAsync());
+        }
+    }
+}
