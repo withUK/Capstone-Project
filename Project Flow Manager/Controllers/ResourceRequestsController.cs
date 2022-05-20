3a863aaca340181da54e69e9982430e75a155f0d
withUK <peter.withington@live.co.uk>
2022-05-20 14:03:19 +0100

    Placeholders added for documentation

--- Project Flow Manager/Controllers/ResourceRequestsController.cs
+++ Project Flow Manager/Controllers/ResourceRequestsController.cs
@@ -18,7 +18,10 @@ namespace Project_Flow_Manager.Controllers
             _context = context;
         }
 
-        // GET: ResourceRequests
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public async Task<IActionResult> Index()
         {
             ViewData["Title"] = "Resourcing";
@@ -27,7 +30,11 @@ namespace Project_Flow_Manager.Controllers
                           Problem("Entity set 'InnovationManagerContext.ResourceRequest'  is null.");
         }
 
-        // GET: ResourceRequests/Details/5
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.ResourceRequest == null)
@@ -46,16 +53,21 @@ namespace Project_Flow_Manager.Controllers
             return View(resourceRequest);
         }
 
-        // GET: ResourceRequests/Create
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Create()
         {
             ViewData["Title"] = "Add a new request";
             return View();
         }
 
-        // POST: ResourceRequests/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="resourceRequest"></param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,ProjectAssessmentId")] ResourceRequest resourceRequest)
@@ -70,7 +82,11 @@ namespace Project_Flow_Manager.Controllers
             return View(resourceRequest);
         }
 
-        // GET: ResourceRequests/Edit/5
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.ResourceRequest == null)
@@ -87,9 +103,12 @@ namespace Project_Flow_Manager.Controllers
             return View(resourceRequest);
         }
 
-        // POST: ResourceRequests/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="resourceRequest"></param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,ProjectAssessmentId")] ResourceRequest resourceRequest)
@@ -123,7 +142,11 @@ namespace Project_Flow_Manager.Controllers
             return View(resourceRequest);
         }
 
-        // GET: ResourceRequests/Delete/5
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.ResourceRequest == null)
@@ -142,7 +165,11 @@ namespace Project_Flow_Manager.Controllers
             return View(resourceRequest);
         }
 
-        // POST: ResourceRequests/Delete/5
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -161,6 +188,11 @@ namespace Project_Flow_Manager.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         private bool ResourceRequestExists(int id)
         {
           return (_context.ResourceRequest?.Any(e => e.Id == id)).GetValueOrDefault();
