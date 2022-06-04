f432c5dbf26f52fcbcdd1b532152414df6e5afb3
withUK <peter.withington@live.co.uk>
2022-06-04 11:10:21 +0100

    Created Initial Test Project

---
+++ ProjectFlowManagerUnitTests/ProjectFlowManagerAdministrationTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace ProjectFlowManagerUnitTests
+{
+    public class ProjectFlowManagerAdministrationTests
+    {
+        #region EffortMeasures
+        [Fact]
+        public void EffortMeasuresController_Index_ReturnsView()
+        {
+
+        }
+        #endregion
+    }
+}
